924d39eb3b409d4d142f3c0ccc052871b5b4d0b7
Marcio Campos <marciomrbcampos@gmail.com>
2021-11-08 13:20:30 -0300

    site

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+var button = document.getElementById('searchButton');
+var searchBox = document.getElementById('searchBox');
+var closeButton = document.getElementById('buttonClose');
+
+var input = document.querySelector('input#procurar');
+var label = document.querySelector('.search label');
+
+var dropMenu = document.querySelector('li.dropdown');
+var dropMenuContent = document.querySelector('ul.dropdown-menu');
+
+dropMenu.addEventListener('click', function() { 
+  if(dropMenuContent.classList.contains('show') == true) {
+    dropMenuContent.classList.remove('show');
+  } else {
+    dropMenuContent.classList.add('show');
+  }
+});
+
+
+function OpenModal() {
+  searchBox.style.display = 'flex';
+}
+function CloseModal() {
+  searchBox.style.display = 'none';
+}
+
+input.addEventListener('focusin', function() { 
+  label.classList.add('d-none');
+});
+input.addEventListener('focusout', function() { 
+  label.classList.remove('d-none');
+});
+
+
+function SendMessage() {
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+
+  if(name == '' || email == '')  {
+    alert('Preencha todos os campos');
+  } else {
+    alert('Mensagem enviada com sucesso');
+  }
+}
+
+
